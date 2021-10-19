42ff79131fdd708f0f3a679c5613b41fd44337c1
jeremiebapte <jeremiebapte89@gmail.com>
2021-10-19 15:29:27 +0200

    1er commit

--- src/test/java/com/parkit/parkingsystem/integration/config/DataBaseTestConfig.java
+++ src/test/java/com/parkit/parkingsystem/integration/config/DataBaseTestConfig.java
@@ -3,18 +3,19 @@ package com.parkit.parkingsystem.integration.config;
 import com.parkit.parkingsystem.config.DataBaseConfig;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.Test;
 
 import java.sql.*;
 
 public class DataBaseTestConfig extends DataBaseConfig {
 
     private static final Logger logger = LogManager.getLogger("DataBaseTestConfig");
-
+@Test
     public Connection getConnection() throws ClassNotFoundException, SQLException {
         logger.info("Create DB connection");
         Class.forName("com.mysql.cj.jdbc.Driver");
         return DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/test","root","rootroot");
+                "jdbc:mysql://localhost:3306/test?serverTimezone=UTC","root","rootroot");
     }
 
     public void closeConnection(Connection con){
